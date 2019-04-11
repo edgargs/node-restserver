a368b5d88880b4599699cc24eacb344e50993395
Edgar Rios Navarro <edgar.gs@gmail.com>
2019-04-10 19:28:53 -0500

    Add mongoose

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -8,29 +9,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res){
-    res.json("Hello World");
+app.use( require('./routes/usuario') );
+
+//Connect Mongo
+mongoose.connect('mongodb+srv://cluster0-d0dpp.mongodb.net/node-db?retryWrites=true',{
+    useNewUrlParser: true,
+    user: 'node-user',
+    pass: 'C1b3rt3c@081'
+  }, (err, res) => {
+    if(err) throw err;
+
+    console.log("BBDD online");
 });
-
-app.post('/usuario', function(req, res){
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "Nombre undefinend"
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res){
-    let id = req.params.id;
-    res.json("Hello World");
-});
-
 
 app.listen(process.env.PORT);
